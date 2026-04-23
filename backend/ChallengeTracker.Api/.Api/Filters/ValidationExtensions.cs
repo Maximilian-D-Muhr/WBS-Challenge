9a51e085074223e262fdf11dcc0c33ea97a32a21
Umi <umiolo1994@gmail.com>
2026-04-23 12:03:43 +0200

    validation with extension method

---
+++ backend/ChallengeTracker.Api/.Api/Filters/ValidationExtensions.cs
@@ -0,0 +1,8 @@
+namespace ChallengeTracker.Api.Api.Filters;
+
+public static class ValidationExtensions
+{
+  public static RouteHandlerBuilder WithValidation<T>(this RouteHandlerBuilder builder)
+      where T : class
+      => builder.AddEndpointFilter(new ValidationFilter<T>());
+}
